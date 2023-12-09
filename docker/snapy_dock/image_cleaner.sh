d9f402eb070dca98f6eecee1e85981301076df18
gvdiaz <gdiaz@veng.com.ar>
2023-12-09 16:27:07 -0300

    Imágenes a borrar de pc de laburo.

--- docker/snapy_dock/image_cleaner.sh
+++ docker/snapy_dock/image_cleaner.sh
@@ -1,13 +1,7 @@
 #! /bin/bash
-
-docker rmi 17c0b7af214a
-docker rmi fb7e7a5cf27f
-docker rmi 1c6b0711b378
-docker rmi c7111a946ab4
-docker rmi a335c33fe3e6
-docker rmi 87e30e88bce3
-docker rmi adbb2dff0788
-docker rmi 0c0b218af55e
-docker rmi 94b1b0789ad6
-docker rmi 0600432918a7
-docker rmi ff5c41254bdc
+# Script para limpiar imágenes sin utilizar en docker local
+docker rmi 05c5646e9759
+docker rmi 66f9e16b9458
+docker rmi ebcea9608463
+docker rmi 1466b844bf45
+docker rmi 307368f1028f
