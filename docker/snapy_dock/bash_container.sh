186a8d1b1e41b67d98a0bab2f0622b3e31ac1efd
gvdiaz <gdiaz@veng.com.ar>
2023-12-09 18:01:52 -0300

    Modificación en llamadores de contenedor y en id_saver para guardar las búsquedas en la carpeta de salida del proyectos y que quede permanentemente

--- docker/snapy_dock/bash_container.sh
+++ docker/snapy_dock/bash_container.sh
@@ -6,6 +6,7 @@ PATH_SCRIPTS="${PATH_REPO}/Scripts"
 PATH_VECTORES="${PATH_REPO}/Vectores"
 PATH_UTILS="${PATH_REPO}/utils"
 PATH_TOKEN="${PATH_REPO}/token"
+PATH_OUTPUT="${PATH_REPO}/output"
 
 # bash en imagen snappy_9_gvd
 docker run --rm --name snappy_develop \
@@ -14,6 +15,7 @@ docker run --rm --name snappy_develop \
     -v $PATH_VECTORES:/src/Vectores \
     -v $PATH_UTILS:/src/utils \
     -v $PATH_TOKEN:/src/token \
+    -v $PATH_OUTPUT:/src/output \
     --workdir /src \
     -p 8888:8888 \
     snappy_9_gvd \
--- docker/snapy_dock/dev_jup.sh
+++ docker/snapy_dock/dev_jup.sh
@@ -18,6 +18,7 @@ PATH_SCRIPTS="${PATH_REPO}/Scripts"
 PATH_VECTORES="${PATH_REPO}/Vectores"
 PATH_UTILS="${PATH_REPO}/utils"
 PATH_TOKEN="${PATH_REPO}/token"
+PATH_OUTPUT="${PATH_REPO}/output"
 
 # echo $PATH_REPO $PATH_SCRIPTS
 docker run --rm --name snappy_jup_develop \
@@ -26,6 +27,7 @@ docker run --rm --name snappy_jup_develop \
     -v $PATH_VECTORES:/src/Vectores \
     -v $PATH_UTILS:/src/utils \
     -v $PATH_TOKEN:/src/token \
+    -v $PATH_OUTPUT:/src/output \
     --workdir /src \
     -p 8888:8888 \
     snappy_9_gvd \
