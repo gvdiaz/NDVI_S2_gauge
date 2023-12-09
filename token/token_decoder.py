cf591ec55623736125cb1c83d06dd95516544567
gvdiaz <gdiaz@veng.com.ar>
2023-12-09 18:26:16 -0300

    Modificaciones para debuggear en notebook el script token_decoder

--- token/token_decoder.py
+++ token/token_decoder.py
@@ -5,7 +5,8 @@ import datetime as dt
 import json
 import requests
 import sys
-import utils.mod_dloader as mdl
+sys.path.append(r'../utils')
+import mod_dloader as mdl
 
 # Argumento 1: Ruta que contiene diccionario token
 # Argumento 2: Ruta que contiene string keycloak
@@ -15,21 +16,23 @@ import utils.mod_dloader as mdl
 mdl.show_sname(sys.argv[0])
 
 # Verifico si se ingresó el archivo de token
-pth_comp = sys.argv[1]
-if not(sys.argv[1]):
-    path_token =r'/home/negrete/test_gee/token/'
+try:
+    pth_comp = sys.argv[1]
+except:
+    path_token =r'./'
     text_file = r'token_dict.txt'
     pth_comp = os.path.join(path_token,text_file)
     print(os.path.exists(pth_comp), pth_comp)
 
 # Verifico archivo de product id
-pth_prod_id = sys.argv[3]
-if not(sys.argv[3]):
-    path_prod_id = r'/mnt/c/Users/DiazGustavoV/Documents/Pers_Proj/Sentinel_2_getter/Output/'
+try:
+    pth_prod_id = sys.argv[3]
+except:
+    path_prod_id = r'../output/prod_list/'
     prod_id_file = r'prod_id_selected.txt'
     pth_prod_id = os.path.join(path_prod_id, prod_id_file)
     print(os.path.exists(pth_prod_id), pth_prod_id)
-
+    
 # Relevo escritura de archivo de token
 with open(pth_comp, mode="r") as file:
     lines = file.readlines()
@@ -46,13 +49,16 @@ print(f'Muestra de keycloak token {kc_token}',os.environ[kc_token],sep='\n')
 print('Muestro token de access token: ',dict_def[aux_key], sep='\n')
 
 # Lectura de archivo que contiene "product id"
-with open(pth_prod_id, 'r') as prod_id_file:
-    # print(prod_id_file.read())
-    prod_id = prod_id_file.read()
-print('Muestra de id de producto:',prod_id, sep='\n')
+# with open(pth_prod_id, 'r') as prod_id_file:
+#     # print(prod_id_file.read())
+#     prod_id = prod_id_file.read()
+# print('Muestra de id de producto:',prod_id, sep='\n')
+
+prod_id = ms.read_list(out_list)[0]
+print(prod_id)
 
 # Directorio de producto de salida
-output_path = r'/home/negrete/test_gee/token/output/'
+output_path = r'../output/'
 
 mdl.prod_downloader(prod_id, os.environ[kc_token],output_path)
 # mdl.prod_downloader(prod_id, dict_def[aux_key], output_path)
