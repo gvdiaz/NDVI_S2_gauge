ea2110206d35c66e5ffb63eaa85a7ff6e719ee31
gvdiaz <gdiaz@veng.com.ar>
2024-04-14 17:36:17 -0300

    Límpio líneas de debug

--- token/token_getter.py
+++ token/token_getter.py
@@ -7,15 +7,14 @@ import mod_dloader as mdl
 # Argumento 2: pass_s2
 # Argumento 3: Ruta de salida para guardar cadena de caracteres de keycloak
 
-# Muestro qué script se encuentra trabajando
-mdl.show_sname(sys.argv[0])
+
 
 # Archivos para pedido de token modelo y de salida
 curl_model_path = './get_token_model.sh'
 curl_output = './get_token.sh'
 
 # Muestro si existe la ruta del archivo modelo
-print(os.path.isfile(curl_model_path), curl_model_path)
+# print(os.path.isfile(curl_model_path), curl_model_path)
 
 # Levanto toda la cadena del archivo modelo
 with open(curl_model_path, 'r') as model_req:
@@ -40,8 +39,8 @@ with open(curl_output,'w') as output_file:
     output_file.write(text2rep)
 
 ## Pedido de keycloak
-print('Pedido de keycloak')
-key_cloak_str = mdl.get_keycloak(sys.argv[1], sys.argv[2])
+# print('Pedido de keycloak')
+key_cloak_str = mdl.get_keycloak(sys.argv[1], sys.argv[2], sys.argv[3])
 
 # print(key_cloak_str, type(key_cloak_str), sep='\n')
 
@@ -49,7 +48,11 @@ key_cloak_str = mdl.get_keycloak(sys.argv[1], sys.argv[2])
 with open(sys.argv[3],'w') as out_file:
     out_file.write(key_cloak_str)
 
-print(f'Fin de script {sys.argv[0]}')
-print()
+if sys.argv[3]:
+    # Muestro qué script se encuentra trabajando
+    mdl.show_sname(sys.argv[0])
+    print(f'Fin de script {sys.argv[0]}')
+    print()
+    
     
 # os.chmod(curl_output, stat.S_IRWXU)
