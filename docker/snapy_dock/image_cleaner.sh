fbd8731fcdb0c36daf807e1c6922918a1ca94c9c
Gustavo Vladimir Diaz <gus838@gmail.com>
2024-08-24 17:20:28 -0300

    Limpio imágenes de docker que se habían generado erróneamente.

--- docker/snapy_dock/image_cleaner.sh
+++ docker/snapy_dock/image_cleaner.sh
@@ -1,7 +1,17 @@
 #! /bin/bash
 # Script para limpiar imágenes sin utilizar en docker local
-docker rmi 05c5646e9759
-docker rmi 66f9e16b9458
-docker rmi ebcea9608463
-docker rmi 1466b844bf45
-docker rmi 307368f1028f
+docker rmi 0872f8b682de
+docker rmi 609ab5ebc6a4
+docker rmi 2ce42b2f6a7b
+docker rmi 958dd37e6f40
+docker rmi 59c3edd832f8
+docker rmi f3dafefd555c
+docker rmi 1ffc6d518fc6
+docker rmi 965dfde04551
+docker rmi c096fd426c72
+docker rmi 6bd0ef2fe102
+docker rmi b15cefbc42bb
+docker rmi 911c9adcbece
+docker rmi 7c10a43a29de
+docker rmi e916c3e978d6
+docker rmi e5730e012672
