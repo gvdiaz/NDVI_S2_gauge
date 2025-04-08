5450019849a307346ff4acc95da829f996fc37d0
Gustavo Vladimir Diaz <gus838@gmail.com>
2025-04-08 13:23:15 -0300

    Implementación de scripts en docker para generar nuevo contenedor, entrar en el comprobar comandos, etc.

---
+++ docker/snappy_11_dock/bash_container.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+# Script en bash para levantar arrancar bash de docker de desarrollo en imagen snappy_9_gvd
+
+PATH_REPO="$(cd ../ && cd ../ && pwd)"
+PATH_SCRIPTS="${PATH_REPO}/Scripts"
+PATH_VECTORES="${PATH_REPO}/Vectores"
+PATH_UTILS="${PATH_REPO}/utils"
+PATH_TOKEN="${PATH_REPO}/token"
+PATH_OUTPUT="${PATH_REPO}/output"
+
+# bash en imagen snappy_11_gvd
+docker run --rm --name snappy_11_develop \
+    -it \
+    -v $PATH_SCRIPTS:/src/Scripts \
+    -v $PATH_VECTORES:/src/Vectores \
+    -v $PATH_UTILS:/src/utils \
+    -v $PATH_TOKEN:/src/token \
+    -v $PATH_OUTPUT:/src/output \
+    --workdir /src \
+    snappy_11_gvd \
+    bash
+    # Agregar siguiente línea para habilitar el puerto 8888:8888 para cuando tenga el notebook funcionando.
+    # -p 8888:8888 \
+    # "jupyter" "notebook" "--allow-root"
+    #'jupyter notebook'
---
+++ docker/snappy_11_dock/builder.sh
@@ -0,0 +1,3 @@
+# /usr/bin/bash
+
+docker build -t snappy_11_gvd -f Dockerfile .
