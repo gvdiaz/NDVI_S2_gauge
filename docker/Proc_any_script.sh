8a139ef9c30b27b9ce37871f9eb7426309d5d40b
Gustavo Vladimir Diaz <gus838@gmail.com>
2025-04-13 15:40:17 -0300

    Procesador para probar funciones de snappy 9 con módulo snappy 11 recientemente compilado.

--- docker/Proc_any_script.sh
+++ docker/Proc_any_script.sh
@@ -7,8 +7,11 @@
 # Configuración para mostrar los comandos cuando se ejecutan
 # set -x
 
+# objetivo script:
+# Pruebas programadas para probar procesador con módulos progamados en snappy 9 python3.6 y correrlos en snappy 11 y python 3.10.
+
 # Monitor de parámetros
-tty
+# tty
 
 # PATHS
 PATH_REPO="$(cd ..&& pwd)"
@@ -16,12 +19,12 @@ PATH_REPO="$(cd ..&& pwd)"
 # PATH_REPO="$(pwd)"
 # PATH_REPO="${}"
 PATH_PY_GIS="${PATH_REPO}/docker/py_GIS_dock/"
-PATH_SNAPPY="${PATH_REPO}/docker/snapy_dock/"
+PATH_SNAPPY="${PATH_REPO}/docker/snappy_11_dock/"
 PROC_CONF="${PATH_REPO}/utils/CONF_PROC.INI"
 PATH_OUTPUT="${PATH_REPO}/output/"
 
 # SCRIPTS BASH
-PYTHON_LAUNCHER="python_36_launcher.sh"
+PYTHON_LAUNCHER="python_310_launcher.sh"
 PROC_SCRIPT="downloader_and_proc.sh"
 SEARCH_SCRIPT="search_filt_runner.sh"
 
@@ -36,21 +39,21 @@ RESUMER="resumer.py"
 # echo "Directorio para repo: $(pwd)"
 # pwd
 # echo ${PATH_REPO}
-# cd ${PATH_PY_GIS}
-# sudo ./${SEARCH_SCRIPT}
+cd ${PATH_PY_GIS}
+sudo ./${SEARCH_SCRIPT}
 # echo "Directorio para py_GIS_dock: $(pwd)"
 # pwd
 # echo ${PATH_PY_GIS}
 cd ${PATH_SNAPPY}
 
-# sudo ./${PYTHON_LAUNCHER} ${FOLDER_CREATOR}
+sudo ./${PYTHON_LAUNCHER} ${FOLDER_CREATOR}
 
-# REL_PATH2LIST=$(sudo ./${PYTHON_LAUNCHER} ${FOLDER_CREATOR})
-# echo "Primera impresión"
+REL_PATH2LIST=$(sudo ./${PYTHON_LAUNCHER} ${FOLDER_CREATOR})
+echo "Primera impresión"
 # echo ${REL_PATH2LIST}
-# PATH2LIST="${PATH_OUTPUT}${REL_PATH2LIST}"
+PATH2LIST="${PATH_OUTPUT}${REL_PATH2LIST}"
 # echo $REL_PATH2LIST
-# echo $PATH2LIST
+echo $PATH2LIST
 
 # if [ -e $PATH2LIST ]; then
 #     echo "File exists."
@@ -58,42 +61,42 @@ cd ${PATH_SNAPPY}
 #     echo "File does not exist."
 # fi
 
-# cat $PATH2LIST
-# echo "Directorio para snapy_dock: $(pwd)"
-# pwd
-# echo ${PATH_SNAPPY}
-
-# while IFS= read -r line; do
-#     echo Línea: $line
-#     # echo $(pwd)
-#     # echo "./${PYTHON_LAUNCHER} ${DOWNLOADER_PROD} ${line}"
-#     ./"$PYTHON_LAUNCHER" "$DOWNLOADER_PROD" "$line"
-#     ./"$PYTHON_LAUNCHER" "$PROC_PROD" "$line"
-#     # echo $LAUNCH_SCRIPT
-#     # $LAUNCH_SCRIPT
-#     # ./${PYTHON_LAUNCHER} ${DOWNLOADER_PROD} ${line}
-#     # DOWNLOADER_RESPONSE=$(sudo -t ./${PYTHON_LAUNCHER} ${DOWNLOADER_PROD} ${line})
-#     # echo $DOWNLOADER_RESPONSE
-#     echo
-# done < $PATH2LIST
-
-./"$PYTHON_LAUNCHER" "$RESUMER"
+cat $PATH2LIST
+echo "Directorio para snapy_dock: $(pwd)"
+pwd
+echo ${PATH_SNAPPY}
 
 while IFS= read -r line; do
-    # echo "Line: $line"
-    if [[ "$line" == prods_list* ]]; then
-        echo $line
-        path="${line}"
-        filename=$(basename "$path")
-        path_bash="${PATH_OUTPUT}${filename}"
-        echo $path_bash
-        line_count="Cantida de lineas en $path_bash"
-        # cat $path_bash
-        echo $line_count
-        lines=$(($(wc -l < "$path_bash") - 1 ))
-        echo $lines
+    echo Línea: $line
+    # echo $(pwd)
+    # echo "./${PYTHON_LAUNCHER} ${DOWNLOADER_PROD} ${line}"
+    ./"$PYTHON_LAUNCHER" "$DOWNLOADER_PROD" "$line"
+    ./"$PYTHON_LAUNCHER" "$PROC_PROD" "$line"
+    # echo $LAUNCH_SCRIPT
+    # $LAUNCH_SCRIPT
+    # ./${PYTHON_LAUNCHER} ${DOWNLOADER_PROD} ${line}
+    # DOWNLOADER_RESPONSE=$(sudo -t ./${PYTHON_LAUNCHER} ${DOWNLOADER_PROD} ${line})
+    # echo $DOWNLOADER_RESPONSE
+    echo
+done < $PATH2LIST
 
-    fi
-done < $PROC_CONF
+# ./"$PYTHON_LAUNCHER" "$RESUMER"
+
+# while IFS= read -r line; do
+#     # echo "Line: $line"
+#     if [[ "$line" == prods_list* ]]; then
+#         echo $line
+#         path="${line}"
+#         filename=$(basename "$path")
+#         path_bash="${PATH_OUTPUT}${filename}"
+#         echo $path_bash
+#         line_count="Cantida de lineas en $path_bash"
+#         # cat $path_bash
+#         echo $line_count
+#         lines=$(($(wc -l < "$path_bash") - 1 ))
+#         echo $lines
+
+#     fi
+# done < $PROC_CONF
 
 # line_count=$(wc -l < "$file")
