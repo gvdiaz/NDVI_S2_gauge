4eba571173547b00c2f9431ae19d49c23421db2f
Gustavo V. Diaz <gus838@gmail.com>
2025-04-29 08:44:19 -0300

    Modificación de scripts para realizar configuración inicial y registro de errores para script python de referencia.

--- Scripts/downloader_prods.py
+++ Scripts/downloader_prods.py
@@ -19,78 +19,87 @@ import mod_dloader as mdl
 
 # Funciones que necesito implementar
 
-# Cuerpo de script a ejecutar
-# Parte 2da
-# Tengo definido ruta a archivo de configación del procesador general.
-config_path = r'/src/utils/CONF_PROC.INI'
-development = False
-verbose2conf = False
+def main(config_path, development, verbose2conf):
+    # Función 1B
+    # Lectura de csv con listado de productos
+    path2csv = conf_dict['FOLDERS']['prods_list']
+    df = mfc.lectura_csv(path2csv, False)
 
-# Función 1A
-# Lectura de archivo de configuración de búsqueda
-conf_dict = mfc.read_conf_proc(config_path, verbose2conf)
+    # print("Presento argumento 1", sys.argv[1])
 
-# Función 1B
-# Lectura de csv con listado de productos
-path2csv = conf_dict['FOLDERS']['prods_list']
-df = mfc.lectura_csv(path2csv, False)
+    # print(f"Muestro dataframe: \n{df}")
+    filt_id = df['Name'] == sys.argv[1]
+    idx_row = df.loc[filt_id].index.to_list()[0]
+    # id = df.loc[filt_id]['Id']
+    print(f'Indice de linea de producto buscado{idx_row}')
+    id = df.at[idx_row, 'Id']
 
-# print("Presento argumento 1", sys.argv[1])
+    print()
+    print(f'Presento id de producto: {sys.argv[1]}\n{id}')
+    print()
+    # print(f'Fila que tiene coincidencia con argumento {sys.argv[1]}\n{row}')
+    # print(f'Presento el tipo de id:\n{type(id)}')
+    # print(f'Y presento el id que necesito:\n{row.get('Id')}')
+    # print(f'Y presento el tipo de dato:\n{type(row)}')
+    # print(f'Y presento el id que necesito:\n{row['Id']}')
 
-# print(f"Muestro dataframe: \n{df}")
-filt_id = df['Name'] == sys.argv[1]
-idx_row = df.loc[filt_id].index.to_list()[0]
-# id = df.loc[filt_id]['Id']
-print(f'Indice de linea de producto buscado{idx_row}')
-id = df.at[idx_row, 'Id']
+    # Recopilación de datos para bajar producto
+    # Definición de usuario
+    user = conf_dict['ATTRIB']['user']
+    passw = conf_dict['ATTRIB']['pass']
 
-print()
-print(f'Presento id de producto: {sys.argv[1]}\n{id}')
-print()
-# print(f'Fila que tiene coincidencia con argumento {sys.argv[1]}\n{row}')
-# print(f'Presento el tipo de id:\n{type(id)}')
-# print(f'Y presento el id que necesito:\n{row.get('Id')}')
-# print(f'Y presento el tipo de dato:\n{type(row)}')
-# print(f'Y presento el id que necesito:\n{row['Id']}')
+    prod_id = df.at[idx_row, 'Id']
+    prod_name = df.at[idx_row, 'Name']
+    acq_date = str(df.at[idx_row, 'acq_date'])
+    str_token = mdl.get_keycloak(user, passw, False)
+    # print(prod_id, prod_name, acq_date, str_token, sep='\n')
+    # Verifico creación de directorios de proyecto
+    root_folder = conf_dict['FOLDERS']['output']
+    print(root_folder)
+    subfolder_name = 'tmp'
+    tmp_path = os.path.join(root_folder, subfolder_name)
+    if os.path.isdir(tmp_path):
+        print(f"Carpeta temporal {tmp_path} creada.")
+    else:
+        print(f"Carpeta temporal {tmp_path} no creada.")
 
-# Recopilación de datos para bajar producto
-# Definición de usuario
-user = conf_dict['ATTRIB']['user']
-passw = conf_dict['ATTRIB']['pass']
+    # if len(os.listdir(tmp_path)) > 2:
+    #     print("Hay mas de un archivo bajado, termino script")
+    #     sys.exit(0)
+    # else:
+    #     pass
 
-prod_id = df.at[idx_row, 'Id']
-prod_name = df.at[idx_row, 'Name']
-acq_date = str(df.at[idx_row, 'acq_date'])
-str_token = mdl.get_keycloak(user, passw, False)
-# print(prod_id, prod_name, acq_date, str_token, sep='\n')
-# Verifico creación de directorios de proyecto
-root_folder = conf_dict['FOLDERS']['output']
-print(root_folder)
-subfolder_name = 'tmp'
-tmp_path = os.path.join(root_folder, subfolder_name)
-if os.path.isdir(tmp_path):
-    print(f"Carpeta temporal {tmp_path} creada.")
-else:
-    print(f"Carpeta temporal {tmp_path} no creada.")
+    out_name = prod_name + '.zip'
+    prod_path = os.path.join(tmp_path, out_name)
+    print(f"Debug downloader_prods.py, nombre de archivo de salida:\n{prod_path}")
 
-# if len(os.listdir(tmp_path)) > 2:
-#     print("Hay mas de un archivo bajado, termino script")
-#     sys.exit(0)
-# else:
-#     pass
+    # Ahora verifico si existe el producto bajado
+    if os.path.isfile(prod_path):
+        print(f"Archivo {prod_path} bajado.")
+    else:
+        print(f"Archivo {prod_path} no bajado.")
+        print("Bajando...")
+        mdl.prod_downloader_2(prod_id, str_token, tmp_path, prod_name, verbose2conf)
 
-out_name = prod_name + '.zip'
-prod_path = os.path.join(tmp_path, out_name)
-print(f"Debug downloader_prods.py, nombre de archivo de salida:\n{prod_path}")
+    # file2verif = os.path.join(tmp_path,prod_name + '.zip')
 
-# Ahora verifico si existe el producto bajado
-if os.path.isfile(prod_path):
-    print(f"Archivo {prod_path} bajado.")
-else:
-    print(f"Archivo {prod_path} no bajado.")
-    print("Bajando...")
-    mdl.prod_downloader_2(prod_id, str_token, tmp_path, prod_name, verbose2conf)
+    # mdl.prod_downloader_2(prod_id, str_token, tmp_path, prod_name, False)
 
-# file2verif = os.path.join(tmp_path,prod_name + '.zip')
 
-# mdl.prod_downloader_2(prod_id, str_token, tmp_path, prod_name, False)
+if __name__ == "__main__":
+    config_path = r'/src/utils/CONF_PROC.INI'
+    development = False
+    verbose2conf = False
+    # Función 1A
+    # Lectura de archivo de configuración de búsqueda
+    conf_dict = mfc.read_conf_proc(config_path, verbose2conf)
+    log_filename = mfc.configure_logging(folder_path = conf_dict['FOLDERS']['logs'], proj_name = conf_dict['ATTRIB']['proj_name'])
+
+    try:
+        exit_code = main(config_path, development, verbose2conf)
+        if os.path.exists(log_filename) and os.path.getsize(log_filename) == 0:
+            os.remove(log_filename)
+    except Exception as e:
+        logging.error(f"Fatal error: {str(e)}", exc_info=True)
+        exit_code = 1
+    sys.exit(exit_code)
